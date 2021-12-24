cb029f89e053db16d2425ed84126893ad59e7bd3
nextdoorwith <nextdoorwithadm@gmail.com>
2021-12-24 21:05:08 +0900

    add HostBuilder example

--- Azure/Functions/FunctionsDotnet5/Program.cs
+++ Azure/Functions/FunctionsDotnet5/Program.cs
@@ -1,9 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace FunctionsDotnet5
@@ -13,12 +11,21 @@ namespace FunctionsDotnet5
         public static Task Main()
         {
             var host = new HostBuilder()
-                .ConfigureFunctionsWorkerDefaults()
-                .ConfigureFunctionsAppConfiguration() // サンプルで独自拡張
+                .ConfigureFunctionsWorkerDefaults() // 必須
+                // 構成情報を追加する場合
+                .ConfigureAppConfiguration((context, config) =>
+                {
+                    var env = context.HostingEnvironment;
+                    config
+                        .SetBasePath(Directory.GetCurrentDirectory())
+                        .AddJsonFile("appsettings.json", optional: true)
+                        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+                })
+                // 依存関係を注入する場合
                 .ConfigureServices((context, services) =>
                 {
                     var configuration = context.Configuration;
-                    services.AddHttpClient();
+                    services.AddHttpClient(); // 例としてHttpClientをDI
                 })
                 .Build();
             return host.RunAsync();
